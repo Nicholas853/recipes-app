44e700d3cc04d2953441444b2a0c8dc35b59c450
Nick <nikolaj.sprinc@gmail.com>
2025-02-27 10:13:57 +0200

    fix(Theme): Fix bug for success Vercel deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from '@/lib/queryClient'; // Імпортуємо queryClient
+import { queryClient } from '@/lib/queryClient';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { theme } from '@/theme';
@@ -31,7 +31,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {/* Обгортаємо додаток у QueryClientProvider та ThemeProvider */}
         <QueryClientProvider client={queryClient}>
           <ThemeProvider theme={theme}>
             <CssBaseline />
